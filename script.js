82ea680c3404bd96f2a2b705da36d4859180a787
mn-hs <mnich.hs@gmail.com>
2022-02-02 09:02:29 -0500

    Added color drawing with selecting button

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 const sketchContainer = document.querySelector('.sketchContainer');
 sketchContainer.classList.add('sketchContainer');
 
+//Storing prompt for gridSize
+gridSize =  ''
+
 
 
 //creates a grid with calles & specified dimensions when called
@@ -17,9 +20,27 @@ function createCells(columns, rows){
     sketchContainer.style.gridTemplateRows = `repeat(${rows}, 20px)`;
   }
 }
-createCells(16, 16);
+createCells(20, 20);
 
+function generateRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
+let randomColor = generateRandomColor();
+
+function colorRandom(){
+  const pixels = document.querySelectorAll(`.gridCell`)
+  pixels.forEach((pixel) => {
+    pixel.addEventListener('mouseover', () => {
+      pixel.style.backgroundColor = randomColor;
+    });
+  });
+}
 
 //changes cell background to black when mouseover
 function colorBlack(){
@@ -31,16 +52,20 @@ function colorBlack(){
   });
 }
 
+
 //invokes colorBlack() when start drawing button is clicked
-//refreshes page when clear sketch-pad button is clicked
-const buttons = document.querySelectorAll('#draw, #clear');
+//refreshes page when clear sketch-pad button is clicked & asks for new grid size
+const buttons = document.querySelectorAll('#drawBlack, #drawColor, #clear');
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
-    if (button.id == 'draw'){
+    if (button.id == 'drawBlack'){
       colorBlack();
     }
-    else if (button.id == 'clear'){
+    else if (button.id == 'clear') {
       window.location.reload();
+    }
+    else if (button.id == 'drawColor'){
+      colorRandom();
     }
   })
 });
